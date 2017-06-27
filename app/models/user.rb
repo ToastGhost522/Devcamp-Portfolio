56a0227d161b5639d2bb0eca02fce4832a9f9fd6
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 17:21:16 +0000

    Implemented basic authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable (Email confirmation), :lockable (Failed login), :timeoutable  and :omniauthable (Sign in through something else)
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
